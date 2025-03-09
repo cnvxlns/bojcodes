f05dd48951475c32624f87a88fe8b6fb8ad5bcea
hyeonK <157864988+okmac03@users.noreply.github.com>
2025-03-09 17:23:49 +0900

    [Silver III] Title: 등차수열을 만들어요, Time: 8 ms, Memory: 2268 KB -BaekjoonHub

--- 백준/Silver/32298. 등차수열을 만들어요/등차수열을 만들어요.cc
+++ 백준/Silver/32298. 등차수열을 만들어요/등차수열을 만들어요.cc
@@ -1,60 +1,56 @@
-#include<bits/stdc++.h>
-
-#define Size 2000010
-#define inf 0x3f3f3f3f
-#define endl "\n"
-#define sp " "
-#define int int64_t
-#define loop(M) for(int _ = 0; _ < M; ++_)
-
-const int MOD = 1e9 + 7;
+#include <iostream>
+#include <vector>
+#include <cmath>
 
 using namespace std;
 
-using vi = vector<int>;
-using pii = pair<int, int>;
-using tii = tuple<int, int, int>;
+const int MAX_N = 2000001;
 
-void Solve();
+vector<bool> sieve(int limit) {
+    vector<bool> is_prime(limit + 1, true);
+    is_prime[0] = is_prime[1] = false;
 
-int32_t main(){
-    cin.tie(0) -> sync_with_stdio(0);
-    int t = 1;
-    // cin >> t;
-    loop(t){
-        Solve();
+    for (int i = 2; i * i <= limit; i++) {
+        if (is_prime[i]) {
+            for (int j = i * i; j <= limit; j += i) {
+                is_prime[j] = false;
+            }
+        }
     }
-    return 0;
+    return is_prime;
 }
 
-void Solve(){
-    int N, M;
-    vi isprime;
-    isprime.assign(Size, 1);
-    isprime[1] = 0;
-    isprime[2] = 1;
-    for(int i = 3; i <= 2000000; ++i){
-        for(int j = 2; i * j <= 2000000; ++j){
-            isprime[i * j] = 0;
-        }
-    }
-    cin >> N >> M;
-    for(int i = 1; i <= 2000000; ++i){
-        vi ans;
-        int flag = 0;
-        for(int j = 0; j < N; ++j){
-            int t = i + j * M;
-            if(t > 2000000 || isprime[t]){
-                flag = 1;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, m;
+    cin >> n >> m;
+
+    vector<bool> is_prime = sieve(MAX_N);
+
+    for (int i = 1; i < MAX_N; i++) {
+        vector<int> arr;
+        bool is_all_composite = true;
+
+        for (int j = 0; j < n; j++) {
+            int num = i + j * m;
+            if (is_prime[num]) {
+                is_all_composite = false;
+                break;
             }
-            ans.push_back(t);
+            arr.push_back(num);
         }
-        if(flag){
-            continue;
+
+        if (is_all_composite) {
+            for (int num : arr) {
+                cout << num << " ";
+            }
+            cout << "\n";
+            return 0;
         }
-        for(auto i: ans){
-            cout << i << sp;
-        }
-        break;
     }
+
+    cout << "-1\n";
+    return 0;
 }
