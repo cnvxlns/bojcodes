f6dcf444370f99fabd20977c7f8e3f36788a4766
Donghyeon Kim <157864988+cnvxlns@users.noreply.github.com>
2025-09-25 11:20:14 +0900

    [Gold IV] Title: N-Queen, Time: 4644 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.cc
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -15,61 +15,59 @@ using namespace std;
 using vi = vector<int>;
 using vvi = vector<vi>;
 using pii = pair<int, int>;
+using pqi = priority_queue<int>;
+using pqig = priority_queue<int, vi, greater<int>>;
 using tiii = tuple<int, int, int>;
 using pic = pair<int, char>;
-using pci = pair<char, int>;    
+using pci = pair<char, int>;
 using vpii = vector<pii>;
 using vpic = vector<pic>;
 using vpci = vector<pci>;
 using vtiii = vector<tiii>;
 
 pii operator+(const pii& a, const pii& b) {
-    return {a.first + b.first, a.second + b.second};
+    return { a.first + b.first, a.second + b.second };
 }
 pii operator-(const pii& a, const pii& b) {
-    return {a.first - b.first, a.second - b.second};
+    return { a.first - b.first, a.second - b.second };
 }
 
-int N;
-int ans;
-vi queen;
+pii dxy[] = { {0, 1}, {1, 0}, {0, -1}, {-1, 0} };
 
-bool check(int row, int col){
-    REP(i, row){
-        if(queen[i] == col || abs(queen[i] - col) == abs(i - row)){
+int N;
+
+bool validity(vi& chk, int row, int col){
+    REP(i, col){
+        if(chk[i] == row || abs(chk[i] - row) == abs(i - col)){
             return false;
         }
     }
     return true;
 }
 
-void bt(int row){
-   
-    if(row == N){
+void dfs(vi& chk, int col, int& ans){
+    if(col == N){
         ans++;
-        return;
     }
-    REP(col, N){
-        if(check(row, col)){
-            queen[row] = col;
-            bt(row + 1);
+    REP(i, N){
+        if(validity(chk, i, col)){
+            chk[col] = i;
+            dfs(chk, col + 1, ans);
         }
     }
 }
 
-void Solve(int t){
+void Solve() {
     cin >> N;
-    queen.assign(N, 0);
-    bt(0);
+    int ans = 0;
+    vi chk(N, 0);
+    dfs(chk, 0, ans);
     cout << ans << endl;
 }
 
-int32_t main(){
-    cin.tie(0) -> sync_with_stdio(0);
-    int t = 1;
-    //cin >> t;
-    rep(i, 1, t){
-        Solve(i);
-    }
+int32_t main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    Solve();
     return 0;
 }
