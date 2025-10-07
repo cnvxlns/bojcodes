9de9cbfcba13af7cb6710f0e98f18599a18a069c
Donghyeon Kim <157864988+cnvxlns@users.noreply.github.com>
2025-10-08 01:28:49 +0900

    [Bronze II] Title: 단어의 개수, Time: 76 ms, Memory: 4916 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.cc
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+
+#define Size 200032
+#define inf 4e18
+#define endl "\n"
+#define sp " "
+#define int int64_t
+
+const int MOD = 1e9 + 7;
+
+using namespace std;
+
+using vi = vector<int>;
+using vvi = vector<vi>;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+using pic = pair<int, char>;
+using pci = pair<char, int>;
+using vpii = vector<pii>;
+using vpic = vector<pic>;
+using vpci = vector<pci>;
+using vtiii = vector<tiii>;
+
+pii operator+(const pii& a, const pii& b) {
+    return { a.first + b.first, a.second + b.second };
+}
+pii operator-(const pii& a, const pii& b) {
+    return { a.first - b.first, a.second - b.second };
+}
+
+pii dxy[] = { {0, 1}, {1, 0}, {0, -1}, {-1, 0} };
+
+void Solve(){
+    string s;
+    int ans = 0;
+    while(cin >> s){
+        ans++;
+    }
+    cout << ans << endl;
+}
+
+int32_t main() {
+    //cin.tie(0)->sync_with_stdio(0);
+    int t = 1;
+    //cin >> t;
+    while(t--)
+        Solve();
+    return 0;
+}
