09e13ccdc0052783c582cdd993fad4e18f82b039
Donghyeon Kim <157864988+cnvxlns@users.noreply.github.com>
2025-11-02 16:26:19 +0900

    [Gold IV] Title: 네트워크 연결, Time: 32 ms, Memory: 6760 KB -BaekjoonHub

--- 백준/Gold/1922. 네트워크 연결/네트워크 연결.cc
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.cc
@@ -1,59 +1,92 @@
 #include<bits/stdc++.h>
 
-#define Size 300
-#define inf 0x3f3f3f3f
+#define int int64_t
+#define Size 200032
+#define inf 4000000000000000000LL 
 #define endl "\n"
 #define sp " "
-#define loop(M) for(int _ = 0; _ < M; ++_)
+
+
+const int MOD = 1e9 + 7;
 
 using namespace std;
 
-using ll = long long;
-using vi = vector<ll>;
-using pii = pair<ll, ll>;
-using tiii = tuple<ll, ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+using pic = pair<int, char>;
+using pci = pair<char, int>;
+using vpii = vector<pii>;
+using vpic = vector<pic>;
+using vpci = vector<pci>;
+using vtiii = vector<tiii>;
 
-const vector<pii> dij = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
-
-int mstprim(const vector<vector<pii>> G, int N){
-    int res = 0;
-    vi visited(N + 1);
-    fill(visited.begin(), visited.end(), 0);
-    priority_queue<pii, vector<pii>, greater<pii>> pq;
-    pq.push({0, 1});
-    while(!pq.empty()){
-        auto [curweight, curnode] = pq.top();
-        pq.pop();
-        if(visited[curnode]){
-            continue;
-        }else{
-            visited[curnode] = 1;
-            res += curweight;
-            for(auto [nextweight, nextnode]: G[curnode]){
-                if(!visited[nextnode]){
-                    pq.push({nextweight, nextnode});
-                }
-            }
-        }
-    }
-    return res; 
+pii operator+(const pii& a, const pii& b) {
+    return { a.first + b.first, a.second + b.second };
+}
+pii operator-(const pii& a, const pii& b) {
+    return { a.first - b.first, a.second - b.second };
 }
 
-void Solve(){
+pii dxy[] = { {0, 1}, {1, 0}, {0, -1}, {-1, 0} };
+
+vi parent;
+
+void init(int size){
+    parent.resize(size + 1);
+    iota(parent.begin(), parent.end(), 0);
+}
+
+int Find(int a){
+    if(a == parent[a]){
+        return a;
+    }
+    return parent[a] = Find(parent[a]);
+}
+
+void Union(int a, int b){
+    a = Find(a), b = Find(b);
+    if(a == b){
+        return ;
+    }else{
+        parent[a] = b;
+    }
+}
+
+void Solve() {
     int N, M;
     cin >> N >> M;
-    vector<vector<pii>> G(N + 1);
-    loop(M){
-        int a, b, w;
-        cin >> a >> b >> w;
-        G[a].push_back({w, b});
-        G[b].push_back({w, a});
+    vtiii lines;
+    while(M--){
+        int a, b, c;
+        cin >> a >> b >> c;
+        lines.push_back({c, a, b});
     }
-    cout << mstprim(G, N) << endl;
+    // cout << "ok" << endl;
+    sort(lines.begin(), lines.end());
+    int ans = 0, cnt = 0;
+    init(N);
+    for(const auto &[val, a, b]: lines){
+        if(Find(a) == Find(b)){
+            continue;
+        }else{
+            ans += val;
+            cnt++;
+            Union(a, b);
+        }
+        if(cnt == N - 1){
+            break;
+        }
+    }
+    cout << ans << endl;
 }
 
-int main() {
-    cin.tie(0) -> sync_with_stdio(0);
-    Solve();
+int32_t main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int t = 1;
+    //cin >> t;
+    while(t--)
+        Solve();
     return 0;
 }
