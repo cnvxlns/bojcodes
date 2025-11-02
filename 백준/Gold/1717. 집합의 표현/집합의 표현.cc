d5d5c3ffa7e2bddf170f4003c10fdc18a4ffe05e
Donghyeon Kim <157864988+cnvxlns@users.noreply.github.com>
2025-11-02 15:27:15 +0900

    [Gold V] Title: 집합의 표현, Time: 36 ms, Memory: 10228 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.cc
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.cc
@@ -1,14 +1,40 @@
-#include<iostream>
-#include<vector>
+#include<bits/stdc++.h>
+
+#define int int64_t
+#define Size 200032
+#define inf 4000000000000000000LL 
+#define endl "\n"
+#define sp " "
+
+
+const int MOD = 1e9 + 7;
 
 using namespace std;
 
-int N, M;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+using pic = pair<int, char>;
+using pci = pair<char, int>;
+using vpii = vector<pii>;
+using vpic = vector<pic>;
+using vpci = vector<pci>;
+using vtiii = vector<tiii>;
 
-vector<int> parent(1000100);
+pii operator+(const pii& a, const pii& b) {
+    return { a.first + b.first, a.second + b.second };
+}
+pii operator-(const pii& a, const pii& b) {
+    return { a.first - b.first, a.second - b.second };
+}
+
+pii dxy[] = { {0, 1}, {1, 0}, {0, -1}, {-1, 0} };
+
+vi parent;
 
 int Find(int a){
-    if(a == parent[a]){
+    if(a == parent[a]){ 
         return a;
     }else{
         return parent[a] = Find(parent[a]);
@@ -16,35 +42,52 @@ int Find(int a){
 }
 
 void Union(int a, int b){
-    a = Find(a);
-    b = Find(b);
-    if(a < b){
-        parent[b] = a;
+    a = Find(a), b = Find(b);
+    if(a == b){
+        return;
+    }else if(a < b){
+        parent[b] = a; 
     }else{
         parent[a] = b;
     }
+}
+
+void init(int size){
+    parent.assign(size + 1, 0);
+    iota(parent.begin(), parent.end(), 0);
+}
+
+
+void Solve() {
+    int n, m;
+    cin >> n >> m;
+
+    init(n);
+
+    while(m--){
+        int query, a, b;
+        cin >> query >> a >> b;
+        if(query == 0){
+            Union(a, b);
+        }   
+        if(query == 1){
+            if(Find(a) == Find(b)){
+                cout << "YES" << endl;
+            }else{
+                cout << "NO" << endl;
+            }
+        }
+    }
+
+
 
 }
 
-int main(){
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cin >> N >> M;
-    for(int i = 1; i <= N; ++i){
-        parent[i] = i;
-    }
-    while(M--){
-        int q, a, b;
-        cin >> q >> a >> b;
-        if(q){
-            if(Find(a) == Find(b)){
-                cout << "yes" << "\n";
-            }else{
-                cout << "no" << "\n";
-            }
-        }else{ 
-            Union(a, b);
-        }
-    }
+int32_t main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int t = 1;
+    //cin >> t;
+    while(t--)
+        Solve();
     return 0;
 }
