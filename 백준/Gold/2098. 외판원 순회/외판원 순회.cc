11ecc0dfaec2f6f74a65589b9654edba019df5db
Donghyeon Kim <157864988+cnvxlns@users.noreply.github.com>
2025-11-09 15:02:18 +0900

    [Gold I] Title: 외판원 순회, Time: 20 ms, Memory: 10796 KB -BaekjoonHub

--- 백준/Gold/2098. 외판원 순회/외판원 순회.cc
+++ 백준/Gold/2098. 외판원 순회/외판원 순회.cc
@@ -1,64 +1,80 @@
 #include<bits/stdc++.h>
 
-#define Size 20
-#define inf 0x3f3f3f3f
+#define int long long
+#define Size 200032
+#define inf (int)4e18
 #define endl "\n"
 #define sp " "
-#define int int64_t
-#define loop(M) for(int _ = 0; _ < M; ++_)
+
 
 const int MOD = 1e9 + 7;
 
 using namespace std;
 
 using vi = vector<int>;
+using vvi = vector<vi>;
 using pii = pair<int, int>;
-using tii = tuple<int, int, int>;
+using tiii = tuple<int, int, int>;
+using pic = pair<int, char>;
+using pci = pair<char, int>;
+using vpii = vector<pii>;
+using vpic = vector<pic>;
+using vpci = vector<pci>;
+using vtiii = vector<tiii>;
 
-void Solve();
-
-int32_t main(){
-    cin.tie(0) -> sync_with_stdio(0);
-    int t = 1;
-    // cin >> t;
-    loop(t){
-        Solve();
-    }
-    return 0;
+pii operator+(const pii& a, const pii& b) {
+    return { a.first + b.first, a.second + b.second };
+}
+pii operator-(const pii& a, const pii& b) {
+    return { a.first - b.first, a.second - b.second };
 }
 
-int N;
-int cost[Size][Size];
-vector<vi> cache;
-
-int TSP(int cur, int visited){
-    if(visited == (1 << N) - 1){
-        if(cost[cur][0]){
-            return cost[cur][0];
-        }else{
-            return inf;
-        }
-    }
-    int &ret = cache[cur][visited];
-    if(ret != -1){
-        return ret;
-    }
-    ret = inf;
-    for(int i = 0; i < N; ++i){
-        if((visited & (1 << i)) == 0 && cost[cur][i] != 0){
-            ret = min(ret, cost[cur][i] + TSP(i, visited | (1 << i)));
-        }
-    }
-    return ret;
-}
+pii dxy[] = { {0, 1}, {1, 0}, {0, -1}, {-1, 0} };
 
 void Solve(){
+    int N;
     cin >> N;
-    cache.assign(N, vi(1 << N, -1));
-    for(int i = 0; i < N; ++i){
-        for(int j = 0; j < N; ++j){
-            cin >> cost[i][j];
+    vvi a(N, vi(N, 0));
+    for(auto &i: a){
+        for(auto &j: i){
+            cin >> j;
+            if(j == 0){
+                j = inf;
+            }
         }
     }
-    cout << TSP(0, 1) << endl;
+    vvi dp(N, vi(1 << N, inf));
+    dp[0][1] = 0;
+    for(auto status = 0; status < (1 << N); ++status){
+        for(auto cur = 0; cur < N; ++cur){
+            if(!(status & (1 << cur))) continue;
+            if(dp[cur][status] == inf) continue;
+            for(auto next = 0; next < N; ++next){
+                if(a[cur][next] == inf || (status & (1 << next))){
+                    continue;
+                }else{
+                    dp[next][status | (1 << next)] = min(dp[next][status | (1 << next)], dp[cur][status] + a[cur][next]);
+                }
+            }
+        }
+    }
+    int ans = inf;
+    for(int i = 0; i < N; ++i){
+        if(dp[i][(1 << N) - 1] != inf && a[i][0] != inf){
+            ans = min(ans, dp[i][(1 << N) - 1] + a[i][0]);
+        }
+    }
+
+    if (N == 1) ans = 0;
+        cout << ans << endl;
+
+}
+
+int32_t main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int t = 1;
+    //cin >> t;
+    while(t--)
+        Solve();
+    return 0;
 }
