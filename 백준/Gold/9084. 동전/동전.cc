c47db106f47aecac2e33b701133119b358bc97cd
Donghyeon Kim <157864988+cnvxlns@users.noreply.github.com>
2025-11-12 22:45:01 +0900

    [Gold V] Title: 동전, Time: 0 ms, Memory: 2212 KB -BaekjoonHub

---
+++ 백준/Gold/9084. 동전/동전.cc
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+
+#define int long long
+#define Size 20000
+#define inf (int)4e18
+#define endl "\n"
+#define sp " "
+
+#define pcll(x) __builtin_popcountll(x)
+
+const int MOD = 1e9 + 7;
+
+using namespace std;
+
+using vi = vector<int>;
+using vvi = vector<vi>;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+using pic = pair<int, char>;
+using pci = pair<char, int>;
+using vpii = vector<pii>;
+using vpic = vector<pic>;
+using vpci = vector<pci>;
+using vtiii = vector<tiii>;
+
+pii operator+(const pii& a, const pii& b) {
+    return { a.first + b.first, a.second + b.second };
+}
+pii operator-(const pii& a, const pii& b) {
+    return { a.first - b.first, a.second - b.second };
+}
+
+pii dxy[] = { {0, 1}, {1, 0}, {0, -1}, {-1, 0} };
+
+void Solve(){
+    int N, K;
+    cin >> N;
+    vi a(N);
+    for(auto &i: a){
+        cin >> i;
+    }
+    cin >> K;
+    
+    vi dp(K + 1);
+    dp[0] = 1;
+    for(auto i: a){
+        for(auto j = i; j <= K; ++j){
+            dp[j] += dp[j - i];
+        }
+    }
+    cout << dp[K] << endl;
+
+}
+
+int32_t main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int t = 1;
+    cin >> t;
+    while(t--)
+        Solve();
+    return 0;
+}
